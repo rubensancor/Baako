2e6caf2cbfb0311c10c0a35a7bc5cfd1c0c2b304
gusy <gusy@gusy-HP>
2016-04-15 12:19:18 +0200

    AuthServer deleted

---
+++ Baako_Server/src/main/java/baako/server/auth/Main.java
@@ -0,0 +1,26 @@
+package baako.server.auth;
+
+import java.rmi.Naming;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		if(System.getSecurityManager()==null){
+			System.setSecurityManager(new SecurityManager());
+		}
+		
+		String serverName = "//"+args[0]+":"+args[1]+"/"+args[2];
+		
+		try{
+			AuthServer a = new AuthServer();
+			Naming.rebind(serverName, a);
+			a.register("Gaizka","1234");
+			a.register("ruben","asdf");			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		System.out.println("Auth Server "+ serverName+ " active and waiting...");
+	}
+}
