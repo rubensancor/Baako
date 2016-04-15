a7db0ce978cab77ef545bd71193fb3ead9d1518a
gusy <gusy@gusy-HP>
2016-04-15 09:16:26 +0200

    1/2 JDO

--- Baako_Server/src/main/java/baako/server/database/Category.java
+++ Baako_Server/src/main/java/baako/server/database/Category.java
@@ -1,8 +1,34 @@
 package baako.server.database;
 
+import java.util.List;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
 public class Category {
 
+	@PrimaryKey
 	private int categoryId;
 	
 	private String name;
+	
+	@Persistent(mappedBy="categories")
+	List<Game> games;
+
+	/**
+	 * @return the categoryId
+	 */
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	
 }
