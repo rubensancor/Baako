25cc97039db045338d33907e60ea329c7e754458
gusy <gusy@gusy-HP>
2016-04-15 09:43:14 +0200

    Database

--- Baako_Server/src/main/java/baako/server/database/Wallet.java
+++ Baako_Server/src/main/java/baako/server/database/Wallet.java
@@ -4,13 +4,14 @@
 package baako.server.database;
 
 import javax.jdo.annotations.Element;
+import javax.jdo.annotations.PrimaryKey;
 
 /**
  * @author gusy
  *
  */
 public class Wallet {
-
+	@PrimaryKey
 	private int walletId;
 	
 	private int cardNumb;
@@ -18,4 +19,35 @@ public class Wallet {
 	
 	@Element(column="USER_ID")
 	private User user;
+
+	/**
+	 * @return the walletId
+	 */
+	public int getWalletId() {
+		return walletId;
+	}
+
+	/**
+	 * @return the cardNumb
+	 */
+	public int getCardNumb() {
+		return cardNumb;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public CardType getType() {
+		return type;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+	
+	
+	
 }
