9ad5bc774d6c575328ae9f792e4a5e3ea0d79f0e
Carlos Perales <Carlos Perales@carlos-lap>
2016-04-20 22:47:13 +0200

    .

--- Baako_Client/src/main/java/baako/client/gui/GUI.java
+++ Baako_Client/src/main/java/baako/client/gui/GUI.java
@@ -237,9 +237,9 @@ public class GUI {
 	JPanel panel_2 = new JPanel();
 	reguisterPanel.add(panel_2);
 	GridBagLayout gbl_panel_2 = new GridBagLayout();
-	gbl_panel_2.columnWidths = new int[]{27, 72, 56, 30, 37, 37, 49, 19, 0};
+	gbl_panel_2.columnWidths = new int[]{27, 72, 56, 37, 37, 49, 19, 0};
 	gbl_panel_2.rowHeights = new int[]{34, 34, 38, 31, 0};
-	gbl_panel_2.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+	gbl_panel_2.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 	gbl_panel_2.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 	panel_2.setLayout(gbl_panel_2);
 	
@@ -253,6 +253,7 @@ public class GUI {
 	
 	JLabel lblDay = new JLabel("Day");
 	GridBagConstraints gbc_lblDay = new GridBagConstraints();
+	gbc_lblDay.anchor = GridBagConstraints.WEST;
 	gbc_lblDay.insets = new Insets(0, 0, 5, 5);
 	gbc_lblDay.gridx = 2;
 	gbc_lblDay.gridy = 1;
@@ -261,9 +262,9 @@ public class GUI {
 	
 	JComboBox comboDay = new JComboBox();
 	GridBagConstraints gbc_comboDay = new GridBagConstraints();
-	gbc_comboDay.anchor = GridBagConstraints.NORTH;
+	gbc_comboDay.anchor = GridBagConstraints.NORTHWEST;
 	gbc_comboDay.insets = new Insets(0, 0, 5, 5);
-	gbc_comboDay.gridx = 4;
+	gbc_comboDay.gridx = 3;
 	gbc_comboDay.gridy = 1;
 	panel_2.add(comboDay, gbc_comboDay);
 	comboDay.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
@@ -271,6 +272,7 @@ public class GUI {
 	
 	JLabel lblMonth = new JLabel("Month");
 	GridBagConstraints gbc_lblMonth = new GridBagConstraints();
+	gbc_lblMonth.anchor = GridBagConstraints.WEST;
 	gbc_lblMonth.insets = new Insets(0, 0, 5, 5);
 	gbc_lblMonth.gridx = 2;
 	gbc_lblMonth.gridy = 2;
@@ -279,9 +281,9 @@ public class GUI {
 	
 	JComboBox comboYear = new JComboBox();
 	GridBagConstraints gbc_comboYear = new GridBagConstraints();
-	gbc_comboYear.anchor = GridBagConstraints.NORTH;
+	gbc_comboYear.anchor = GridBagConstraints.NORTHWEST;
 	gbc_comboYear.insets = new Insets(0, 0, 5, 5);
-	gbc_comboYear.gridx = 4;
+	gbc_comboYear.gridx = 3;
 	gbc_comboYear.gridy = 2;
 	panel_2.add(comboYear, gbc_comboYear);
 	comboYear.setModel(new DefaultComboBoxModel(new String[] {"2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000", "1999", "1998", "1997", "1996", "1995", "1994", "1993", "1992", "1991", "1990", "1989", "1988", "1987", "1986", "1985", "1984", "1983", "1982", "1981", "1980", "1979", "1978", "1977", "1976", "1975", "1974", "1973", "1972", "1971", "1970", "1969", "1968", "1967", "1966", "1965", "1964", "1963", "1962", "1961", "1960", "1959", "1958", "1957", "1956", "1955", "1954", "1953", "1952", "1951", "1950", "1949", "1948", "1947", "1946", "1945", "1944", "1943", "1942", "1941", "1940", "1939", "1938", "1937", "1936", "1935", "1934", "1933", "1932", "1931", "1930", "1929", "1928", "1927", "1926", "1925", "1924", "1923", "1922", "1921", "1920", "1919", "1918", "1917", "1916", "1915", "1914", "1913", "1912", "1911", "1910", "1909", "1908", "1907", "1906", "1905", "1904", "1903", "1902", "1901", "1900"}));
@@ -289,6 +291,7 @@ public class GUI {
 	
 	JLabel lblYear = new JLabel("Year");
 	GridBagConstraints gbc_lblYear = new GridBagConstraints();
+	gbc_lblYear.anchor = GridBagConstraints.WEST;
 	gbc_lblYear.insets = new Insets(0, 0, 0, 5);
 	gbc_lblYear.gridx = 2;
 	gbc_lblYear.gridy = 3;
@@ -297,9 +300,9 @@ public class GUI {
 	
 	JComboBox comboMonth = new JComboBox();
 	GridBagConstraints gbc_comboMonth = new GridBagConstraints();
-	gbc_comboMonth.anchor = GridBagConstraints.NORTH;
+	gbc_comboMonth.anchor = GridBagConstraints.NORTHWEST;
 	gbc_comboMonth.insets = new Insets(0, 0, 0, 5);
-	gbc_comboMonth.gridx = 4;
+	gbc_comboMonth.gridx = 3;
 	gbc_comboMonth.gridy = 3;
 	panel_2.add(comboMonth, gbc_comboMonth);
 	comboMonth.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
