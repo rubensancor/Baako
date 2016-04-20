9db2d5fea7dde19180d0c16d08461892709233ba
gusy <gusy@gusy-HP>
2016-04-20 23:00:10 +0200

    DAO & YouTrack

---
+++ Baako_Server/src/main/java/baako/server/dto/DTOAssembler.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package baako.server.dto;
+
+/**
+ * @author gusy
+ *
+ */
+public class DTOAssembler {
+
+//	public static UserDTO assemble(User user){
+//		return new UserDTO(user);
+//	}
+}
