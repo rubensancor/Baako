50cdbbb597332b7528c338cf9ddfb69d6e1c244c
Gaizka Virumbrales <gvirum@opendeusto.es>
2016-05-03 14:06:01 +0200

    t

--- Baako_Server/src/test/java/baako/server/FuncionTest.java
+++ Baako_Server/src/test/java/baako/server/FuncionTest.java
@@ -1,34 +1,38 @@
 package baako.server;
 
+import org.junit.Test;
+
+import baako.server.auth.Auth;
+
 public class FuncionTest {
 
-//	@BeforeClass 
-//  public static void setUpClass() throws Exception { 
-//      // Code executed before the first test method        
-//  } 
-//	@Before 
-//  public void setUp() throws Exception { 
-//      // Code executed before each test     
-//  } 
-//  @Test 
-//  public void testOneThing() { 
-//      // Code that tests one thing 
-//  } 
-//  @Test 
-//  public void testAnotherThing() { 
-//      // Code that tests another thing 
-//  } 
-//  @Test 
-//  public void testSomethingElse() { 
-//      // Code that tests something else 
-//  } 
-//  @After 
-//  public void tearDown() throws Exception { 
-//      // Code executed after each test    
-//  } 
-//  @AfterClass 
-//  public static void tearDownClass() throws Exception { 
-//      // Code executed after the last test method
-//  } 
+	//	@BeforeClass 
+	//  public static void setUpClass() throws Exception { 
+	//      // Code executed before the first test method        
+	//  } 
+	//	@Before 
+	//  public void setUp() throws Exception { 
+	//      // Code executed before each test     
+	//  } 
+	@Test 
+	public void testChechInfo() { 
+		Auth a = new Auth(dao) 
+	} 
+	//  @Test 
+	//  public void testAnotherThing() { 
+	//      // Code that tests another thing 
+	//  } 
+	//  @Test 
+	//  public void testSomethingElse() { 
+	//      // Code that tests something else 
+	//  } 
+	//  @After 
+	//  public void tearDown() throws Exception { 
+	//      // Code executed after each test    
+	//  } 
+	//  @AfterClass 
+	//  public static void tearDownClass() throws Exception { 
+	//      // Code executed after the last test method
+	//  } 
 
 }
