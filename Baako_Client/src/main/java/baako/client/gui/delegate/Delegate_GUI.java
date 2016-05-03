d65c889043652779fa21ede3d3b71b50c4373b9f
rubensancor <rubensancor@opendeusto.es>
2016-05-03 17:18:59 +0200

    Debugging

--- Baako_Client/src/main/java/baako/client/gui/delegate/Delegate_GUI.java
+++ Baako_Client/src/main/java/baako/client/gui/delegate/Delegate_GUI.java
@@ -19,9 +19,9 @@ public class Delegate_GUI extends GUI{
 	private BaakoController controller;
 
 	public Delegate_GUI(BaakoController controller){
-		this.controller = controller;
+			this.controller = controller;
 	}
-
+	
 	public boolean logIn(String username, String password){
 		if(controller.logIn(username, password)){
 			new Delegate_MainWindow();
@@ -31,19 +31,21 @@ public class Delegate_GUI extends GUI{
 			return false;
 		}
 	}
-
+	
 	public boolean  register(String email, String username, String password, Date date){
+		System.out.println("VAMOS A REGISTRAR");
 		PlainUserDTO u = new PlainUserDTO(email, username, password, date, null, null);
+		System.out.println(u.getEmail());
 		controller.register(u);
-		//		if(controller.register(username, password)){
-		//			return true;
-		//		}else{
-		//			new JOptionPane("YOU ARE ALREADY REGISTERED");
-		//			return false;
-		//		}
+//		if(controller.register(username, password)){
+//			return true;
+//		}else{
+//			new JOptionPane("YOU ARE ALREADY REGISTERED");
+//			return false;
+//		}
 		return true;
 	}
-
-
+	
+	
 
 }
