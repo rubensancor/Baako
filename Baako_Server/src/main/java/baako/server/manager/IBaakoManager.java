7bae3951ba583cfe671e354e5aff38a9f4052c53
rubensancor <rubensancor@opendeusto.es>
2016-05-15 23:56:04 +0200

    Still comments

--- Baako_Server/src/main/java/baako/server/manager/IBaakoManager.java
+++ Baako_Server/src/main/java/baako/server/manager/IBaakoManager.java
@@ -16,69 +16,79 @@ import baako.server.dto.PlainUserDTO;
  */
 public interface IBaakoManager extends Remote{
 
-	/**
-	 * @param username
-	 * @param password
-	 * @return
+	/** Check the user information
+	 * @param username A String with the username of the User to retrieve
+	 * @param password A String with the password of the User to retrieve
+	 * @return A PlainUserDTO with the data of the user retrieved
 	 * @throws RemoteException
 	 */
 	public PlainUserDTO checkUserInfo(String username, String password) throws RemoteException;
-	/**
-	 * @param user
-	 * @return
+	
+	/** Register the user
+	 * @param user A PlainUserDTO to register
+	 * @return A boolean. True if the user is registered and false if not.
 	 * @throws RemoteException
 	 */
 	public boolean register(PlainUserDTO user)throws RemoteException;
-	/**
-	 * @param g
-	 * @param u
-	 * @return
+	
+	/** 
+	 * @param g A GameDTO which has been bought by a User
+	 * @param u The PlainUserDTO who 
+	 * @return 
 	 * @throws RemoteException
 	 */
 	public boolean buyGame(GameDTO g, PlainUserDTO u) throws RemoteException;
-	/**
-	 * @return
+	
+	/** Launch a game 
+	 * @return A boolean. True if the game is launched and false if not.
 	 * @throws RemoteException
 	 */
 	public boolean launchGame() throws RemoteException;
+	
 	/**
 	 * @param name
 	 * @return
 	 * @throws RemoteException
 	 */
 	public GameDTO searchGame(String name) throws RemoteException;
+	
 	/**
 	 * @param category
 	 * @return
 	 * @throws RemoteException
 	 */
 	public ArrayList<GameDTO> searchGamebyCategory(String category) throws RemoteException;
+	
 	/**
 	 * @param designer
 	 * @return
 	 * @throws RemoteException
 	 */
 	public ArrayList<GameDTO> searchGamebyDesigner(String designer) throws RemoteException;
-	/**
-	 * @param cardNumber
-	 * @param u
-	 * @return
+	
+	/** Add a Wallet to the database
+	 * @param cardNumber An integer with the card number.
+	 * @param u A PlainUserDTO  with the data of the User who owns the Wallet
+	 * @return A boolean. True if the wallet is added, false if not
 	 * @throws RemoteException
 	 */
 	public boolean addWallet(int cardNumber, PlainUserDTO u) throws RemoteException;
-	/**
-	 * @param g
-	 * @return
+	
+	/** Add a Game to the database
+	 * @param g A GameDTO to add to the database
+	 * @return A boolean. True if the game is added and false if not.
 	 * @throws RemoteException
 	 */
 	public boolean addGame(GameDTO g) throws RemoteException;
-	/**
-	 * @return All the categories from the DB
+	
+	/** Returns all the Category from the database
+	 * @return All the categories from the DB An ArrayList with all the Category from the database
 	 * @throws RemoteException
 	 */
 	public ArrayList<String> getAllCategories() throws RemoteException;
-	/**
-	 * @return All the Designers from the DB
+	
+	/** Returns all the Designer from the database
+	 * @return All the Designers from the DB An ArrayList with all the Designers from the database
 	 * @throws RemoteException
 	 */
 	public ArrayList<String> getAllDesigners() throws RemoteException;
